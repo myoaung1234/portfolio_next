4343e10d57d8ef1956e32efcd41e7601fc22bb94
myoaung1234 <108474627+myoaung1234@users.noreply.github.com>
2023-03-29 15:35:19 +0630

    change footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
           <Experience />
           <Work />
           <Contant />
-          <Footer />
+          {/* <Footer /> */}
       </div>
     </>
   )
